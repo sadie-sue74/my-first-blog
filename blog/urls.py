c72a5a029fd549d3a15c8413fe5ede08fab02b9d
sadie-sue74 <cwelch20@ivytech.edu>
2025-10-08 08:58:28 -0400

    Added view and templae for detailed blog post as well as CSF for site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
-## importing django path and views
 from django.urls import path
 from . import views
 
-## Django will set our website name as post_list
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
