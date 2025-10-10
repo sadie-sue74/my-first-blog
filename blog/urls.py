7a7dc84e52785102276dff33a86898684e1ba33c
sadie-sue74 <cwelch20@ivytech.edu>
2025-10-10 15:23:21 -0400

    Adding the additonal security, delete, and various other options

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('admin/', admin.site.urls),
+    path('post/new/', views.post_new, name='post_new'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='./logout.html'), name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render, redirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from . import views
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,6 +29,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -39,18 +44,22 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method=='POST':
         post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method=='POST':
+    if request.method == 'POST':
         post.delete()
-    return redirect('post_list')
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
